0e4fd278e4ecac141354a7c55a1ef264162b60eb
volooptaz K <volooptaz@riseup.net>
2021-01-11 19:18:16 -0300

    Mudanças no traverse.py p melhor saída Sphinx
    - Ações ganham headings H3-H6, com aninhamento quando `seq` e `mix` são
    usados
    - Headings são numerados durante a construção do doc, facilitando o
    agrupamento visual de ações dentro de um mesmo `mix`, e `seq`,
    especialmente conforme eles se aninham entre si.
    - Linebreaks após o fim das  `section`s (que levam headin H2)
    - Coloca títulos nos `mix` se  `name`  for uma chave válida.

--- traverse.py
+++ traverse.py
@@ -4,60 +4,74 @@ from rstcloth import RstCloth
 
 doc = RstCloth(line_width=180)
 
-def parse_seq(action, indent=0):
+def parse_seq(action, index, indent):
+    index2 = (1,)
+
     for subaction in action['seq']:
-        parse_action(subaction, indent)
-    doc.newline()
-    doc.newline()
+        parse_action(subaction, index + index2, indent)
+        index2 = index2[:-1] + (index2[-1]+1,)
 
-def parse_mix(action, indent=0):
-    doc.directive('line-block', content='MIX →')
-    doc.newline()
+def parse_mix(action, index, indent):
+    index2 = (1,)
     for subaction in action['mix']:
-        parse_action(subaction, indent+1)
-    doc.directive('line-block', content='←')
-    doc.newline()
+        parse_action(subaction, index + index2, indent=indent+1)
+        index2 = index2[:-1] + (index2[-1]+1,)
 
-def parse_action(action, indent=0):
+HEADINGS = (doc.h3, doc.h4, doc.h5, doc.h6)
+
+def parse_action(action, index=(0,), indent=0):
     assert type(action) == dict
+    index_str = '.'.join(map(str, index[1:])) + '. '
 
-    if 'name' in action:
-        if 'type' in action and action['type'] == 'section':
-            doc.h2(action['name'])
+    if 'type' in action and action['type'] == 'section':
+        if index_str != '1. ':
+            doc._add('\n\n----------\n\n')
+        doc.h2(index_str + action['name'])
+        doc.newline()
+        parse_seq(action, index, indent)
+    elif 'seq' in action:
+        parse_seq(action, index, indent)
+    elif 'mix' in action:
+        heading = HEADINGS[indent]
+        name = 'name' in action and action['name'] or 'Mix'
+        heading(index_str + name + ':')
+        doc.newline()
+        if 'desc' in action:
+            doc.directive('line-block', content=action['desc'])
+        parse_mix(action, index, indent)
+        doc.newline()
+        doc.newline()
+    else:
+        heading = HEADINGS[indent]
+        if 'name' in action:
+            heading(index_str + action['name'])
             doc.newline()
-        else:
-            doc.directive('line-block', content=action['name'])
+
+        if 'url' in action and action['url']:
+            assert 'type' in action
+
+            pre = ''
+            if action['type'] == 'audio':
+                pre += '🔊 '
+            elif action['type'] == 'video':
+                pre += '🎥 '
+            elif action['type'] == 'av':
+                pre += '🎥🔊 '
+            elif action['type'] == 'screen':
+                pre += '🖼️ '
+
+            doc.directive('line-block', content=pre+action['url'])
+            doc.newline()
             doc.newline()
 
-    if 'url' in action and action['url']:
-        assert 'type' in action
 
-        pre = ''
-        if action['type'] == 'audio':
-            pre += '🔊 '
-        elif action['type'] == 'video':
-            pre += '🎥 '
-        elif action['type'] == 'av':
-            pre += '🎥🔊 '
-        elif action['type'] == 'screen':
-            pre += '🖼️ '
-
-        doc.directive('line-block', content=pre+action['url'])
-        doc.newline()
-        doc.newline()
+        if 'type' in action and action['type'] == 'speech':
+            assert 'val' in action
+            pre = '💬 '
+            doc.directive('epigraph', content=pre+action['val'])
+            doc.newline()
 
 
-    if 'type' in action and action['type'] == 'speech':
-        assert 'val' in action
-        pre = '💬 '
-        doc.directive('epigraph', content=pre+action['val'])
-        doc.newline()
-
-    if 'seq' in action:
-        parse_seq(action, indent)
-
-    if 'mix' in action:
-        parse_mix(action, indent)
 
 if __name__ == '__main__':
     with open('liber-ada.yml', 'r') as liber_ada:
